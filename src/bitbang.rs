980a3f66fcbb4ab0352982303626ce5bc83b1c4a
melvdl <hansw533@gmail.com>
2024-09-24 01:08:32 +0200

    implement bitbang SPI and QSPI

--- src/bitbang.rs
+++ src/bitbang.rs
@@ -1,9 +1,10 @@
 use core::cmp::max;
 
-use embassy_stm32::{gpio, Peripheral};
+use embassy_stm32::{gpio, qspi, Peripheral};
 use embassy_time::{block_for, Duration};
 use itertools::Itertools;
 
+#[derive(Debug)]
 #[derive(Clone, Copy)]
 #[derive(PartialEq, Eq)]
 pub enum Cpol {
@@ -11,11 +12,20 @@ pub enum Cpol {
     _1,
 }
 
+#[derive(Debug)]
 #[derive(Clone, Copy)]
 #[derive(PartialEq, Eq)]
 pub enum Cpha {
     _0,
     _1,
+}
+
+#[derive(Debug)]
+#[derive(Copy, Clone)]
+#[derive(Eq, PartialEq)]
+pub enum Mode {
+    Single,
+    Quad,
 }
 
 pub struct Spi<'d> {
@@ -28,6 +38,324 @@ pub struct Spi<'d> {
     sck: gpio::Output<'d>,
     mosi: gpio::Output<'d>,
     miso: gpio::Input<'d>,
+}
+
+pub struct QuadSpi<'d> {
+    min_sck_half_cycle: Duration,
+    cs_high_time: Duration,
+    #[allow(unused)]
+    cpol: Cpol,
+    cpha: Cpha,
+    ncs: gpio::Output<'d>,
+    sck: gpio::Output<'d>,
+    d0_mosi: gpio::Flex<'d>,
+    d1_miso: gpio::Flex<'d>,
+    d2_nwp: gpio::Flex<'d>,
+    d3_nhold: gpio::Flex<'d>,
+}
+
+pub struct QuadTransfer {
+    instruction: Option<(u8, Mode)>,
+    address: Option<(u32, Mode, qspi::enums::AddressSize)>,
+    data: Option<Mode>,
+    dummy_cycles: usize,
+}
+
+enum Direction<'a> {
+    Read(&'a mut [u8]),
+    Write(&'a [u8]),
+}
+
+impl<'d> QuadSpi<'d> {
+    // transmission methods suffixed by an underscore (e.g., [single_transmit_byte_]
+    // do not engage or disengage chip select.
+
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        max_sck_freq: Duration,
+        cs_high_time: Duration,
+        cpol: Cpol,
+        cpha: Cpha,
+        ncs: impl Peripheral<P = impl gpio::Pin> + 'd,
+        sck: impl Peripheral<P = impl gpio::Pin> + 'd,
+        d0_mosi: impl Peripheral<P = impl gpio::Pin> + 'd,
+        d1_miso: impl Peripheral<P = impl gpio::Pin> + 'd,
+        d2_nwp: impl Peripheral<P = impl gpio::Pin> + 'd,
+        d3_nhold: impl Peripheral<P = impl gpio::Pin> + 'd,
+    ) -> Self {
+        let ncs = gpio::Output::new(ncs, gpio::Level::High, gpio::Speed::VeryHigh);
+        let sck = gpio::Output::new(sck, cpol.idle(), gpio::Speed::VeryHigh);
+        let d0_mosi = gpio::Flex::new(d0_mosi);
+        let d1_miso = gpio::Flex::new(d1_miso);
+        let d2_nwp = gpio::Flex::new(d2_nwp);
+        let d3_nhold = gpio::Flex::new(d3_nhold);
+
+        let min_sck_half_cycle = max(Duration::from_ticks(1), max_sck_freq / 2);
+        let cs_high_time = max(Duration::from_ticks(1), cs_high_time);
+
+        let mut qspi = Self {
+            min_sck_half_cycle,
+            cs_high_time,
+            cpol,
+            cpha,
+            ncs,
+            sck,
+            d0_mosi,
+            d1_miso,
+            d2_nwp,
+            d3_nhold,
+        };
+
+        qspi.single_mode();
+
+        qspi
+    }
+
+    pub fn single_read(&mut self, rx: &mut [u8]) {
+        self.single_transfer(&[], rx);
+    }
+
+    pub fn single_write(&mut self, tx: &[u8]) {
+        self.single_transfer(tx, &mut []);
+    }
+
+    pub fn single_transfer(&mut self, tx: &[u8], rx: &mut [u8]) {
+        self.single_mode();
+
+        if self.cpha == Cpha::_1 {
+            block_for(self.min_sck_half_cycle);
+        }
+
+        self.single_transmit_(tx, rx);
+
+        if self.cpha == Cpha::_0 {
+            block_for(self.min_sck_half_cycle);
+        }
+
+        self.ncs.set_high();
+        block_for(self.cs_high_time);
+    }
+
+    pub fn quad_read(&mut self, data: &mut [u8], transfer: &QuadTransfer) {
+        self.quad_transfer(Direction::Read(data), transfer);
+    }
+
+    pub fn quad_write(&mut self, data: &mut [u8], transfer: &QuadTransfer) {
+        self.quad_transfer(Direction::Write(data), transfer);
+    }
+
+    fn quad_transfer(&mut self, direction: Direction, transfer: &QuadTransfer) {
+        self.single_mode();
+
+        if self.cpha == Cpha::_1 {
+            block_for(self.min_sck_half_cycle);
+        }
+
+        if let Some((instruction, mode)) = transfer.instruction {
+            self.transfer_(Direction::Write(&[instruction]), mode);
+        }
+
+        if let Some((address, mode, size)) = transfer.address {
+            let address = &address.to_be_bytes()[match size {
+                | qspi::enums::AddressSize::_8Bit => 3,
+                | qspi::enums::AddressSize::_16Bit => 2,
+                | qspi::enums::AddressSize::_24bit => 1,
+                | qspi::enums::AddressSize::_32bit => 0,
+            }..];
+            self.transfer_(Direction::Write(address), mode);
+        }
+
+        for _ in 0..transfer.dummy_cycles {
+            self.dummy_cycle();
+        }
+
+        if let Some(mode) = transfer.data {
+            self.transfer_(direction, mode);
+        }
+
+        if self.cpha == Cpha::_0 {
+            block_for(self.min_sck_half_cycle);
+        }
+
+        self.ncs.set_high();
+        block_for(self.cs_high_time);
+    }
+
+    fn transfer_(&mut self, direction: Direction, mode: Mode) {
+        match mode {
+            | Mode::Single => {
+                self.single_mode();
+                match direction {
+                    | Direction::Read(data) => self.single_read_(data),
+                    | Direction::Write(data) => self.single_write_(data),
+                }
+            }
+            | Mode::Quad => match direction {
+                | Direction::Read(data) => {
+                    self.quad_read_mode();
+                    self.quad_read_(data);
+                }
+                | Direction::Write(data) => {
+                    self.quad_write_mode();
+                    self.quad_write_(data);
+                }
+            },
+        }
+    }
+
+    fn single_mode(&mut self) {
+        self.d0_mosi.set_low();
+        self.d0_mosi.set_as_output(gpio::Speed::VeryHigh);
+        self.d1_miso.set_as_input(gpio::Pull::Down);
+
+        self.d2_nwp.set_high();
+        self.d2_nwp.set_as_output(gpio::Speed::VeryHigh);
+        self.d3_nhold.set_high();
+        self.d3_nhold.set_as_output(gpio::Speed::VeryHigh);
+    }
+
+    fn quad_read_mode(&mut self) {
+        self.d0_mosi.set_as_input(gpio::Pull::Down);
+        self.d1_miso.set_as_input(gpio::Pull::Down);
+        self.d2_nwp.set_as_input(gpio::Pull::Down);
+        self.d3_nhold.set_as_input(gpio::Pull::Down);
+    }
+
+    fn quad_write_mode(&mut self) {
+        self.d0_mosi.set_low();
+        self.d1_miso.set_low();
+        self.d2_nwp.set_low();
+        self.d3_nhold.set_low();
+        self.d0_mosi.set_as_output(gpio::Speed::VeryHigh);
+        self.d1_miso.set_as_output(gpio::Speed::VeryHigh);
+        self.d2_nwp.set_as_output(gpio::Speed::VeryHigh);
+        self.d3_nhold.set_as_output(gpio::Speed::VeryHigh);
+    }
+
+    fn single_read_(&mut self, rx: &mut [u8]) {
+        self.single_transmit_(&[], rx);
+    }
+
+    fn single_write_(&mut self, tx: &[u8]) {
+        self.single_transmit_(tx, &mut []);
+    }
+
+    fn single_transmit_(&mut self, tx: &[u8], rx: &mut [u8]) {
+        let discard = &mut 0;
+        for x in tx.iter().copied().zip_longest(rx.iter_mut()) {
+            let (tx, rx) = x.or(0, discard);
+            *rx = self.single_transmit_byte_(tx);
+        }
+    }
+
+    fn single_transmit_byte_(&mut self, tx: u8) -> u8 {
+        let mut rx = 0;
+        for bit_pos in (0..8).rev() {
+            if self.cpha == Cpha::_1 {
+                self.sck.toggle();
+            }
+
+            self.d0_mosi.set_level(gpio::Level::from(tx >> bit_pos & 1 == 1));
+            block_for(self.min_sck_half_cycle);
+
+            self.sck.toggle();
+            rx |= (self.d1_miso.get_level() as u8) << bit_pos;
+            block_for(self.min_sck_half_cycle);
+
+            if self.cpha == Cpha::_0 {
+                self.sck.toggle();
+            }
+        }
+        rx
+    }
+
+    fn dummy_cycle(&mut self) {
+        if self.cpha == Cpha::_1 {
+            self.sck.toggle();
+        }
+
+        block_for(self.min_sck_half_cycle);
+        self.sck.toggle();
+        block_for(self.min_sck_half_cycle);
+
+        if self.cpha == Cpha::_0 {
+            self.sck.toggle();
+        }
+    }
+
+    fn quad_read_(&mut self, rx: &mut [u8]) {
+        for rx in rx {
+            *rx = self.quad_read_byte_();
+        }
+    }
+
+    fn quad_write_(&mut self, tx: &[u8]) {
+        for tx in tx {
+            self.quad_write_byte_(*tx);
+        }
+    }
+
+    fn quad_write_byte_(&mut self, tx: u8) {
+        for half in [1, 0] {
+            if self.cpha == Cpha::_1 {
+                self.sck.toggle();
+            }
+
+            let tx = (tx >> (half * 4)) & 0b1111;
+            for (shift, pin) in [
+                &mut self.d0_mosi,
+                &mut self.d1_miso,
+                &mut self.d2_nwp,
+                &mut self.d3_nhold,
+            ]
+            .into_iter()
+            .enumerate()
+            {
+                let level = gpio::Level::from((tx >> shift) & 1 == 1);
+                pin.set_level(level);
+            }
+
+            block_for(self.min_sck_half_cycle);
+            self.sck.toggle();
+            block_for(self.min_sck_half_cycle);
+
+            if self.cpha == Cpha::_0 {
+                self.sck.toggle();
+            }
+        }
+    }
+
+    fn quad_read_byte_(&mut self) -> u8 {
+        let mut rx = 0;
+        for half in [1, 0] {
+            if self.cpha == Cpha::_1 {
+                self.sck.toggle();
+            }
+
+            block_for(self.min_sck_half_cycle);
+            self.sck.toggle();
+
+            for (shift, pin) in [
+                &mut self.d0_mosi,
+                &mut self.d1_miso,
+                &mut self.d2_nwp,
+                &mut self.d3_nhold,
+            ]
+            .into_iter()
+            .enumerate()
+            {
+                rx |= (pin.is_high() as u8) << shift;
+            }
+            rx <<= 4 * half;
+
+            block_for(self.min_sck_half_cycle);
+
+            if self.cpha == Cpha::_0 {
+                self.sck.toggle();
+            }
+        }
+        rx
+    }
 }
 
 impl<'d> Spi<'d> {
@@ -78,7 +406,7 @@ impl<'d> Spi<'d> {
         let discard = &mut 0;
         for x in tx.iter().copied().zip_longest(rx.iter_mut()) {
             let (tx, rx) = x.or(0, discard);
-            self.transmit_byte(tx, rx, self.cpha);
+            *rx = self.transmit_byte(tx, self.cpha);
         }
 
         if self.cpha == Cpha::_0 {
@@ -88,8 +416,8 @@ impl<'d> Spi<'d> {
         block_for(self.cs_high_time);
     }
 
-    fn transmit_byte(&mut self, tx: u8, rx: &mut u8, cpha: Cpha) {
-        *rx = 0;
+    fn transmit_byte(&mut self, tx: u8, cpha: Cpha) -> u8 {
+        let mut rx = 0;
         for bit_pos in (0..8).rev() {
             if cpha == Cpha::_1 {
                 self.sck.toggle();
@@ -99,13 +427,15 @@ impl<'d> Spi<'d> {
             block_for(self.min_sck_half_cycle);
 
             self.sck.toggle();
-            *rx |= (self.miso.get_level() as u8) << bit_pos;
+            rx |= (self.miso.get_level() as u8) << bit_pos;
             block_for(self.min_sck_half_cycle);
 
             if cpha == Cpha::_0 {
                 self.sck.toggle();
             }
         }
+
+        rx
     }
 }
 
