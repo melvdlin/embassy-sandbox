a37017e023c12f2793e2fe95ba8fa7d3dd3c009c
melvdl <hansw533@gmail.com>
2025-02-01 19:00:33 +0100

    move lints from lib.rs to cargo manifest

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,13 @@
 #![no_std]
 #![feature(new_range_api)]
-#![allow(clippy::manual_range_patterns)]
-#![allow(internal_features)]
-#![feature(core_intrinsics)]
 #![feature(sync_unsafe_cell)]
-#![deny(unused_must_use)]
 
 #[cfg(any())]
 pub mod bitbang;
+
 #[cfg(any())]
 pub mod flash;
+
 #[cfg(feature = "cross")]
 pub mod tftp;
 
